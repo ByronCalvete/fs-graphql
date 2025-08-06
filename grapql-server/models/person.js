c3378047ed378bfd983a5c05a2270af9a3518756
Byron J Calvete <calvete94@gmail.com>
2025-08-06 20:47:19 +0200

    add: Database and user administration, part1

---
+++ grapql-server/models/person.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 5
+  },
+  phone: {
+    type: String,
+    minLength: 5
+  },
+  street: {
+    type: String,
+    required: true,
+    minLength: 5
+  },
+  city: {
+    type: String,
+    required: true,
+    minLength: 5
+  }
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+module.exports = Person
