a77fdda818d9fd6f6956b44b0507b07f618251bf
Byron J Calvete <calvete94@gmail.com>
2025-08-11 15:27:33 +0200

    add: exercise 8.19 Books by genre, part1

--- exercise-books-frontend/src/components/Books.jsx
+++ exercise-books-frontend/src/components/Books.jsx
@@ -1,18 +1,28 @@
+import { useState } from 'react'
 import { useQuery } from '@apollo/client'
 import { ALL_BOOKS } from '../queries'
 
 const Books = () => {
   const result = useQuery(ALL_BOOKS)
+  const [ filterGenre, setFilterGenre ] = useState(null)
 
   if (result.loading) {
     return <div>Fetching books...</div>
   }
 
   const books = result.data.allBooks
+  const genres = [ ...new Set((books.map(book => book.genres)).flat()) ]
+
+  const handleGenre = (genre) => {
+    setFilterGenre(genre)
+  }
+
+  const filterBooks = !filterGenre ? books : books.filter(book => book.genres.includes(filterGenre))
 
   return (
     <>
       <h2>books</h2>
+      {filterGenre && <div>in genre <strong>{filterGenre}</strong></div>}
       <table>
         <tbody>
           <tr>
@@ -20,7 +30,7 @@ const Books = () => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map(book => 
+          {filterBooks.map(book => 
             <tr key={book.id}>
               <td>{book.title}</td>
               <td>{book.author.name}</td>
@@ -29,6 +39,14 @@ const Books = () => {
           )}
         </tbody>
       </table>
+      <div style={{ paddingTop: 10 }}>
+        {genres.map(genre =>
+          <button key={genre} onClick={() => handleGenre(genre)}>
+            {genre}
+          </button>
+        )}
+        <button onClick={() => setFilterGenre(null)}>all genres</button>
+      </div>
     </>
   )
 }
